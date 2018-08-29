2fb08370355c0d87ad2f2f2d414d4c2d83bad94c
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-08-29 17:44:44 -0400

    Add splash page with style

--- components/Header.js
+++ components/Header.js
@@ -3,32 +3,56 @@ import Link from 'next/link';
 const Header = () => (
   <div>
     <head>
-      <title> PennClubReview </title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Penn Labs</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" />
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      <link rel="stylesheet" href="styles/style.css" />
+      <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     </head>
 
-    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-      <div class="navbar-menu" id="navMenu">
-        <div class="navbar-start">
-          <Link href="/">
-            <a class="navbar-item">
-              <strong> Penn Labs </strong>
-            </a>
-          </Link>
-          <Link href="/about">
-            <a class="navbar-item">
-              About
-            </a>
-          </Link>
+    <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img
+            src="/img/beaker-only.png"
+            alt="Penn Labs"
+            height="100px"
+            width="auto"
+          />
+        </a>
+
+        <div class="button navbar-burger is-info" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
 
-      <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
+      <div class="navbar-menu" id="navMenu">
+        <div class="navbar-start"></div>
+        <div class="navbar-end">
+          <a class="navbar-item" href="/team">
+            Team
+          </a>
+          <a class="navbar-item" href="/products">
+            Products
+          </a>
+          <a class="navbar-item" href="/api">
+            API
+          </a>
+          <a class="navbar-item" href="/guides">
+            Guides
+          </a>
+          <a class="navbar-item" href="https://medium.com/@pennappslabs">
+            Blog
+          </a>
+          <a class="navbar-item" href="/apply">
+            <strong>Apply</strong>
+          </a>
+        </div>
+      </div>
     </nav>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,34 @@ import Header from '../components/Header';
 const Index = (props) => (
   <div>
     <Header />
-    <section class="hero is-medium is-info is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Penn Labs
-          </h1>
+    <section class="section">
+      <div class="container">
+        <div class="columns ">
+          <div class="column is-6">
+            <h1 class="title">
+            Hi, we're Penn Labs:
+            </h1>
+            <h2 class="subtitle">
+            a team of student <strong>software engineers</strong>, <strong>product designers</strong>, and <strong>business developers.</strong>
+            </h2>
+            <p>
+            Penn Labs empowers others to make connections: connections to resources, connections to people, and connections to the greater Penn community.
+            </p>
+            <br />
+            <p>
+            Our ultimate goal is improving the Penn community. We aim to do so not only by creating high-quality products, but also by giving back to the community with educational resources and technical support.
+            </p>
+            <br />
+            <a href="/products" class="button is-medium"> View Products </a>
+          </div>
+          <div class="column is-5">
+            <figure class="image">
+            <img src="/img/test-hero.png" />
+            </figure>
+          </div>
         </div>
       </div>
     </section>
-    <br />
   </div>
 );
 
