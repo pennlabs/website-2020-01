f2b670c35e05b2d2f0b41aa3dd24361abd3f3b23
Eva Killenberg <evakill@seas.upenn.edu>
2018-11-11 10:42:52 -0500

    no bugs

--- pages/splash_sections/intro.js
+++ pages/splash_sections/intro.js
@@ -1,29 +1,19 @@
 import { Spring, Transition } from 'react-spring';
 
-const items = ["Hi", "we're", "Penn", "Labs!"];
 
 const Intro = (props) => (
   <div className="section">
     <div className="container">
       <div className="columns">
         <div className="column is-6" style={{paddingTop:"3rem"}}>
-          // <Transition
-          //   items={items} keys={item => item.key}
-          //   from={{ transform: 'translate3d(0,-40px,0)' }}
-          //   enter={{ transform: 'translate3d(0,0px,0)' }}
-          //   leave={{ transform: 'translate3d(0,-40px,0)' }}>
-          //   {item => props =>
-          //     <div style={props}>{item.text}</div>
-          //   }
-          // </Transition>
-        // <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={100}>
-        //   {props => (
-        //     <div style={props}>
-        //       <h1 className="title" style={{fontSize:"2.8rem"}}>
-        //         Hi, we're Penn Labs:
-        //       </h1>
-        //     </div>)}
-        // </Spring>
+          <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={100}>
+            {props => (
+              <div style={props}>
+                <h1 className="title" style={{fontSize:"2.8rem"}}>
+                  Hi, we're Penn Labs:
+                </h1>
+              </div>)}
+          </Spring>
           <div>
             <h2 className="subtitle" style={{fontSize:"2rem"}}>
               a team of student <strong>software engineers</strong>, <strong>product designers</strong>, and <strong>business developers.</strong>
@@ -37,12 +27,12 @@ const Intro = (props) => (
             </p>
             <br />
             <a href="/products" className="button is-medium"> View Products </a>
-            <div className="column is-5" style={{paddingLeft:"4rem", marginLeft:"2rem"}}>
-              <figure className="image">
-                <img src="/static/img/test-hero.png" />
-              </figure>
-            </div>
           </div>
+        </div>
+        <div className="column is-5" style={{paddingLeft:"4rem", marginLeft:"2rem"}}>
+          <figure className="image">
+            <img src="/static/img/test-hero.png" />
+          </figure>
         </div>
       </div>
     </div>
