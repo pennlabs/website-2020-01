259fbaca387d70b707a483dbe226390e91e7ce3f
Davis Haupt <davis.haupt@gmail.com>
2018-12-10 16:55:39 -0500

    fixed flicker by moving CSS server-side

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { Spring, Keyframes, config } from 'react-spring'
 import React from 'react'
-
+import '../styles/styles.sass'
 class Header extends React.Component {
   constructor(props){
     super(props);
@@ -21,13 +21,12 @@ class Header extends React.Component {
     return (
       <div>
         <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Penn Labs</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <meta charset="utf-8" />
           <link rel="stylesheet" href="styles/style.css" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         </head>
 
         <nav className="navbar is-info" aria-label="main navigation">
