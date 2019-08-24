957f5b5ee030b63753e98ed292a44cd6840c3e79
valenciafu <valenciafu@gmail.com>
2019-08-24 10:27:58 -0400

    Update apply page

--- pages/apply.js
+++ pages/apply.js
@@ -8,237 +8,178 @@ const Apply = (props) => (
       <div class="hero-body">
         <div class="container">
           <h1 class="title" style={{color: "azure", fontSize: "2.8rem"}}>
-            Build with us.
+            Come build with us!
           </h1>
           <h2 class="subtitle">
-            We're always looking for people to join Labs!
+            We're always looking for people to join Penn Labs.
           </h2>
+          <a class="button is-large is-info is-rounded is-inverted is-focused" href="https://airtable.com/shrdwaiRWZoqtr29O">Apply Now <span class="icon is-small" style={{marginLeft: "0.1rem"}}><i class="fa fa-angle-double-right"></i></span></a>
         </div>
       </div>
     </section>
 
     <section class="section">
-      <div class="container">
-        <h1 class="title" style={{fontSsize: "2.8rem"}}>
-          Why Labs?
+      <div class="container" style={{width: "75%"}}>
+        <h1 class="title" style={{fontSize: "2.4rem"}}>
+          Why Penn Labs?
         </h1>
-        <h2 class="subtitle" style={{fontSize: "2rem"}}>
-          Because we <span class="icon is-small" style={{color: "#F56F71", marginLeft: "1rem", marginRight: "1rem"}}><i class="fa fa-heart"></i></span> creators.
+        <h2 class="subtitle" style={{fontSize: "1.2rem"}}>
+          Because we <span class="icon is-small" style={{color: "#F56F71", marginLeft: "0.3rem", marginRight: "0.3rem"}}><i class="fa fa-heart"></i></span> creators.
         </h2>
-        
-        <div class="columns  ">
-          <div class="column is-half">
-            <div class="box">
-              <div class="card-content">
-                <div class="content">
-                  <h3>
-                    <strong>Iconic Products</strong>
-                  </h3>
-                  <p style={{paddingTop: "0px"}}>
-                    From Penn Course Review to the Common Funding Application, we build software that improves the lives of every Penn student.
-                    Watching your friends use the products that you helped build is a great way to stay grounded and involved in the community.
-                  </p>
-                </div>
-              </div>
-            </div>
 
-            <div class="box">
-              <div class="card-content">
-                <div class="content">
-                  <h3>
-                    <strong>Modern Engineering</strong>
-                  </h3>
-                  <p style={{paddingTop: "0px"}}>
-                    We use exclusively open source modern frameworks that are commonly used in the software industry (i.e. React.js, Django, Swift).
-                    Our engineers are kept up to date with good coding/deployment practices and we always place an emphasis on becoming better programmers.
-                  </p>
-                </div>
+        <p>
+          Penn Labs builds products that help students navigate life at Penn. We're a team of software engineers, product designers, and business developers.
+          <br/><br/> Some products that you may be familiar with include Penn Course Review, Penn Mobile, and Penn Clubs. We’ve had more than 100,000 total users of our products. Through the Common Funding Application that we build and maintain, we've processed over $930,000 in grants for student clubs and organizations.
+        </p>
               </div>
-            </div>
-          </div>
-
-          <div class="column is-half">
-            <div class="box">
-              <div class="card-content">
-                <div class="content">
-                  <h3>
-                    <strong>Top Talent</strong>
-                  </h3>
-                  <p style={{paddingTop: "0px"}}>
-                    Although skill levels vary when joining Labs, our current members are at the top of their game. Through Penn Labs' 
-                    strong work culture our members are highly sought after, becoming teaching assistants,
-                    working for prestigious companies around the world, and launching their own startups.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div class="box">
-              <div class="card-content">
-                <div class="content">
-                  <h3>
-                    <strong>Lots of Fun</strong>
-                  </h3>
-                  <p style={{paddingTop: "0px"}}>
-                    Penn Labs is a relaxed club whose primary goal is to make good software. We pride ourselves on being a tight-knit community of friends 
-                    that support each other. Penn Labs frequently holds hangouts and events that are a great way to avoid thinking about bugs.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-          </div>
     </section>
-
-    <section class="section" style={{backgroundColor: "#209CEE", color: "azure"}}>
-      <div class="container">
-        <h1 class="title" style={{color: "azure", fontSize: "2.8rem"}}>
-          Recruiting for:
-        </h1>
-        <br />
-        <div class="columns">
-          <div class="column">
-            <a href="https://penncoursereview.com/">
-              <div class="box">
-                <div class="card-content">
-                  <div class="level">
-                    <div class="level-left">
-                      <div class="level-item">
-                        <p class="image is-64x64">
-                          <img src="/static/img/PCR.png" />
-                        </p>
-                      </div>
-                      <div class="level-item">
-                        <div class="content">
-                          <h3 style={{paddingTop: "0.5rem", lineHeight: 0, fontWeight: 500}}>
-                            Penn Course Review
-                          </h3>
-                          <p style={{paddingTop: "0px"}}>
-                            Aggregated ratings of Penn courses and professors
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a href="http://pennbasics.com/">
-              <div class="box" style={{marginTop: "1rem"}}>
-                <div class="card-content">
-                  <div class="level">
-                    <div class="level-left">
-                      <div class="level-item">
-                        <p class="image is-64x64">
-                          <img src="/static/img/PennBasics.png" />
-                        </p>
-                      </div>
-                      <div class="level-item">
-                        <div class="content">
-                          <h3 style={{paddingTop: "0.5rem", lineHeight: 0, fontWeight: 500}}>
-                            Penn Basics
-                          </h3>
-                          <p style={{paddingTop: "0px"}}>
-                            Dining, laundry, study space information for desktop
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="column">
-            <a href="https://penncoursealert.com/">
-              <div class="box">
-                <div class="card-content">
-                  <div class="level">
-                    <div class="level-left">
-                      <div class="level-item">
-                        <p class="image is-64x64">
-                          <img src="/static/img/PCA.png" />
-                        </p>
-                      </div>
-                      <div class="level-item">
-                        <div class="content">
-                          <h3 style={{paddingTop: "0.5rem", lineHeight: 0, fontWeight: 500}}>
-                            Penn Course Alert
-                          </h3>
-                          <p style={{paddingTop: "0px"}}>
-                            Get notified when courses become available
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <div class="box" style={{marginTop: "1rem"}}>
-              <div class="card-content">
-                <div class="level">
-                  <div class="level-left">
-                    <div class="level-item">
-                      <p class="image is-64x64">
-                        <img src="/static/img/PennMobile.png" />
+        
+        <section class="section" style={{backgroundColor: "#209CEE", color: "azure"}}>
+          <div class="container" style={{width: "75%"}}>
+            <h1 class="title" style={{fontSize: "2.4rem", color: "azure"}}>
+              Open Roles
+            </h1>
+            <div class="columns" style={{marginTop: "2rem"}}>
+              <div class="column is-half">
+                <div class="box">
+                  <div class="card-content">
+                    <div class="content">
+                      <h3>
+                        <strong>Software Engineering</strong>
+                      </h3>
+                      <p style={{paddingTop: "0px"}}>
+                        From Penn Course Review to the Common Funding Application, we build software that improves the lives of every Penn student.
+                        Watching your friends use the products that you helped build is a great way to stay grounded and involved in the community.
                       </p>
                     </div>
-                    <div class="level-item">
-                      <div class="content">
-                        <h3 style={{paddingTop: "0.5rem", lineHeight: 0, fontWeight: 500}}>
-                          Penn Mobile
-                        </h3>
-                        <p style={{paddingTop: "0px"}}>
-                          Dining, laundry, room reservations for <a href="https://itunes.apple.com/us/app/pennmobile/id944829399?mt=8">iOS</a> and <a href="https://play.google.com/store/apps/details?id=com.pennapps.labs.pennmobile&hl=en">Android</a>
-                        </p>
-                      </div>
+                  </div>
+                </div>
+
+                <div class="box">
+                  <div class="card-content">
+                    <div class="content">
+                      <h3>
+                        <strong>Business Development</strong>
+                      </h3>
+                      <p style={{paddingTop: "0px"}}>
+                        Our products need to be taken from "lab" to real world.  spearheads marketing, events, and collaborations to ensure that Penn Labs reaches real users and meets their needs. If you want to be in the tech industry and help grow an organization in all capacities (strategy, user acquisition, finance, etc), this is the role for you.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="column is-half">
+                <div class="box">
+                  <div class="card-content">
+                    <div class="content">
+                      <h3>
+                        <strong>Product Design</strong>
+                      </h3>
+                      <p style={{paddingTop: "0px"}}>
+                        Connect function with form to reach users and meet their needs through UI/UX and graphic design. Designers create mockups, prototypes, and graphic assets. They also manage the visual language of the organization. 
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <p>
-          ...and many others. Work on one of our iconic products, or a newly-launched one, or start your own initiative.
-        </p>
-      </div>
-    </section>
+        </section>
+
 
     <section class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column">
-            <h1 class="title">
-              What We're Looking For
-            </h1>
-            <div class="content">
-              <blockquote>All Penn students are eligible to apply for any position, regardless of major or year.</blockquote>
-              <p>We're looking for individuals with the following skillsets:</p>
-              <span class="tag is-info">Front End Development</span> <br />
-              <span class="tag is-info">Back End Development</span><br />
-              <span class="tag is-info">Mobile Development</span><br />
-              <span class="tag is-info">DevOps and Security</span><br />
-              <span class="tag is-info">Design (UI/UX and graphic)</span><br />
-              <span class="tag is-info">Business Development</span>
-              <br /> <br />
-              <p>More importantly, we're looking for dedicated students with the capacity to learn. We understand that the best members can come from anywhere, no prior experience necessary!
-              </p>
-            </div>
-            
-          </div>
-        </div>
+      <div class="container" style={{width: "75%"}}>
+        <h1 class="title" style={{fontSize: "2.4rem"}}>
+          Application Process
+        </h1>
+        <h2 class="subtitle">
+            Written application is due <b>Monday 9/9 by 11:59pm</b>
+          </h2>
+        <p>
+          If any of that gets you excited, we’d love for you to <a href="https://airtable.com/shrdwaiRWZoqtr29O">apply to Penn Labs</a>! After getting to know you on paper, we invite candidates to our second round. This takes place in two parts:
+        </p>
+          <ul>
+          <li>
+            <b>One-on-one chat</b> with a current member, where you can ask questions and learn more about the organization
+          </li>
+          <li>
+           <b>Technical challenge</b> where you can show off your skills
+          </li>
+          </ul>
       </div>
     </section>
 
     <section class="section" style={{backgroundColor: "#209CEE", color: "azure"}}>
-      <div class="container">
-        <p style={{color: "azure", fontSize: "1.5rem", paddingBottom: "1rem"}}><b>Join our mailing list</b> to be notified when applications are released:</p>
-        <a href="https://eepurl.com/c07CGb" class="button">Sign me up!</a>
+      <div class="container" style={{width: "75%"}}>
+        <h1 class="title" style={{fontSize: "2.4rem", color:"azure"}}>
+          More about Us
+        </h1>
+        <div class="columns" style={{marginTop: "2rem"}}>
+            <div class="column is-half">
+              <div class="box">
+                <div class="card-content">
+                  <div class="content">
+                    <h3>
+                      <strong>Iconic Products</strong>
+                    </h3>
+                    <p style={{paddingTop: "0px"}}>
+                      From Penn Course Review to the Common Funding Application, we build software that improves the lives of every Penn student.
+                      Watching your friends use the products that you helped build is a great way to stay grounded and involved in the community.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="card-content">
+                  <div class="content">
+                    <h3>
+                      <strong>Modern Engineering</strong>
+                    </h3>
+                    <p style={{paddingTop: "0px"}}>
+                      We use exclusively open source modern frameworks that are commonly used in the software industry (i.e. React.js, Django, Swift).
+                      Our engineers are kept up to date with good coding/deployment practices and we always place an emphasis on becoming better programmers.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-half">
+              <div class="box">
+                <div class="card-content">
+                  <div class="content">
+                    <h3>
+                      <strong>Top Talent</strong>
+                    </h3>
+                    <p style={{paddingTop: "0px"}}>
+                      Although skill levels vary when joining Labs, our current members are at the top of their game. Through Penn Labs' 
+                      strong work culture our members are highly sought after, becoming teaching assistants,
+                      working for prestigious companies around the world, and launching their own startups.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="card-content">
+                  <div class="content">
+                    <h3>
+                      <strong>Lots of Fun</strong>
+                    </h3>
+                    <p style={{paddingTop: "0px"}}>
+                      Penn Labs is a relaxed club whose primary goal is to make good software. We pride ourselves on being a tight-knit community of friends 
+                      that support each other. Penn Labs frequently holds hangouts and events that are a great way to avoid thinking about bugs.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
       </div>
     </section>
+
     <Footer />
   </div>
 );
