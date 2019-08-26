ae0fd559b460fa1e45eb2ea9f51b8c50020542e6
Davis Haupt <davis.haupt@gmail.com>
2019-08-26 07:43:57 -0400

    added google analytics

--- components/Header.js
+++ components/Header.js
@@ -27,6 +27,15 @@ class Header extends React.Component {
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" href="/static/img/favicon.ico" type="image/png/ico" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21029575-11"></script>
+          <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-21029575-11');
+          `}} />
+
         </head>
 
         <nav className="navbar is-info" aria-label="main navigation">
