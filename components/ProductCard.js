3d2fae8e9b287cdde232f633f59097608363ae61
ccabo1 <ccabo@seas.upenn.edu>
2019-10-03 10:15:48 -0400

    refactor to use hooks

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,63 +1,62 @@
+import React, { useState } from 'react'
 import posed from 'react-pose'
-import React from 'react'
 
 const Square = posed.div({
   idle: { scale: 1 },
   hovered: { scale: 1.05 },
 })
 
-class ProductCard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hovering: false,
-    }
-  }
+const ProductCard = ({
+  href,
+  img,
+  title,
+  titleChild,
+  tag,
+  descChild,
+  desc,
+}) => {
+  const [hovering, setHovering] = useState(false)
 
-  render() {
-    return (
-      <div className="column is-half">
-        <Square
-          pose={this.state.hovering ? 'hovered' : 'idle'}
-          onMouseEnter={() => this.setState({ hovering: true })}
-          onMouseLeave={() => this.setState({ hovering: false })}
-        >
-          <a href={this.props.href}>
-            <div className="card" style={{ borderRadius: 5, borderWidth: 0 }}>
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                    {this.props.img ? (
-                      <figure className="image is-64x64">
-                        <img src={this.props.img} alt="Placeholder image" />
-                      </figure>
-                    ) : (
-                      <div></div>
-                    )}
-                  </div>
-                  <div className="media-content">
-                    <div className="content">
-                      <div style={{ paddingBottom: '0.5rem' }}>
-                        {this.props.titleChild ? (
-                          this.props.titleChild
-                        ) : (
-                          <p className="title is-4">{this.props.title}</p>
-                        )}
-                        {this.props.tag}
-                      </div>
-                      {this.props.descChild
-                        ? this.props.descChild
-                        : this.props.desc}
+  return (
+    <div className="column is-half">
+      <Square
+        pose={hovering ? 'hovered' : 'idle'}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      >
+        <a href={href}>
+          <div className="card" style={{ borderRadius: 5, borderWidth: 0 }}>
+            <div className="card-content">
+              <div className="media">
+                <div className="media-left">
+                  {img ? (
+                    <figure className="image is-64x64">
+                      <img src={img} alt="Placeholder image" />
+                    </figure>
+                  ) : (
+                    <div />
+                  )}
+                </div>
+                <div className="media-content">
+                  <div className="content">
+                    <div style={{ paddingBottom: '0.5rem' }}>
+                      {titleChild ? (
+                        titleChild
+                      ) : (
+                        <p className="title is-4">{title}</p>
+                      )}
+                      {tag}
                     </div>
+                    {descChild ? descChild : desc}
                   </div>
                 </div>
               </div>
             </div>
-          </a>
-        </Square>
-      </div>
-    )
-  }
+          </div>
+        </a>
+      </Square>
+    </div>
+  )
 }
 
 export default ProductCard
