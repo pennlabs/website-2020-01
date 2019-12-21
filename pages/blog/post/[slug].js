1c9f905fd58c1ba14de9d9e1a22a7d0fa56c7b35
Davis Haupt <davis.haupt@gmail.com>
2019-12-20 23:16:29 -0400

    pass SEO props for blog to the layout component

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -3,66 +3,51 @@ import { getPost } from '../../../api/ghost'
 import Link from 'next/link'
 import Head from 'next/head'
 
-const Post = ({ post }) => (
-  <Layout>
-    <Head>
-      <link rel="stylesheet" href="/static/css/blog-post.css" />
-      {post.feature_image ? (
-        <>
-          <meta property="og:image" content={post.feature_image} key="image" />
-          <meta
-            property="twitter:image"
-            content={post.feature_image}
-            key="twitter-image"
-          />
-        </>
-      ) : (
-        <></>
-      )}
-      <title key="title">{post.title}</title>
-      <meta property="og:title" content={post.title} key="og-title" />
-      <meta property="twitter:title" content={post.title} key="twitter-title" />
-      <meta
-        name="description"
-        content={post.custom_excerpt || post.excerpt}
-        key="description"
-      />
-      <meta
-        property="twitter:description"
-        content={post.custom_excerpt || post.excerpt}
-        key="twitter-description"
-      />
-    </Head>
-    <section className="section">
-      <Link href="/blog">
-        <a>Back to blog</a>
-      </Link>
+const Post = ({ post }) => {
+  let seoProps = {
+    title: post.title,
+    description: post.custom_excerpt || post.excerpt,
+    url: 'https://pennlabs.org/blog',
+  }
+  if (post.feature_image) {
+    seoProps.image = post.feature_image
+    seoProps.imageAlt = title
+  }
+  return (
+    <Layout {...seoProps}>
+      <Head>
+        <link rel="stylesheet" href="/static/css/blog-post.css" />
+      </Head>
+      <section className="section">
+        <Link href="/blog">
+          <a>Back to blog</a>
+        </Link>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <div className="post-container">
-          <h1 className="title is-2">{post.title}</h1>
-          <img
-            style={{ borderRadius: '2px', margin: '0.8em 0 2.3em' }}
-            src={post.feature_image}
-          />
-          <div className="post-full-content content">
-            <div
-              className="post-content"
-              dangerouslySetInnerHTML={{
-                __html: `
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div className="post-container">
+            <h1 className="title is-2">{post.title}</h1>
+            <img
+              style={{ borderRadius: '2px', margin: '0.8em 0 2.3em' }}
+              src={post.feature_image}
+            />
+            <div className="post-full-content content">
+              <div
+                className="post-content"
+                dangerouslySetInnerHTML={{
+                  __html: `
 ${post.codeinjection_head || ''}
 ${post.html}
 ${post.codeinjection_foot || ''}
                 `,
-              }}
-            />
+                }}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Layout>
-)
-
+      </section>
+    </Layout>
+  )
+}
 Post.getInitialProps = async ({ query }) => {
   const post = await getPost(query.slug)
   return { post }
